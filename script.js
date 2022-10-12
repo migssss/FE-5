5353b4cda1e8e8a8e691120ecc802625d79bd039
migssss <miguelhernandez.mlp@gmail.com>
2022-10-12 13:41:04 -0400

    added more javascript events to multiple webpages, will continue to finish up this project by tomorrow morning 10/13/22

---
+++ script.js
@@ -0,0 +1,154 @@
+/* slideshow event on 'more of me' webpage . . . . . . . . . . . . */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+/* MOUSE CURSOR EVENT . . . . . .. */
+
+
+/* 404 ERROR . . . . . . . . .  */
+
+var changeEl = $(".el");
+var trimLine = anime.timeline({
+  loop: true
+});
+
+trimLine
+  .add({
+    targets: ".el",
+    width: [0, 160],
+    duration: 800,
+    easing: "easeOutQuart",
+    delay: 50
+})
+  .add({
+    targets: ".el",
+    width: [160, 160],
+    easing: "linear",
+    duration: 800
+})
+  .add({
+    targets: ".el",
+    width: [160, 0],
+    duration: 800,
+    easing: "easeOutQuart",
+    delay: 50
+})
+  .add({
+    targets: ".el",
+    width: [0, 0],
+    easing: "linear",
+    duration: 800
+});
+
+trimLine.update = function(anim) {
+  if (anim.currentTime < 2500 && anim.currentTime > 850) {
+    changeEl.css({ float: "right", left: "auto", right: "20px" });
+  }
+
+  if (anim.currentTime > 2500) {
+    changeEl.css({ float: "left", left: "20px", right: "auto" });
+  }
+};
+
+/* button ANIMATION . . . . . . . . . . . .  .. . . . . .  */
+
+Result Skip Results Iframe
+EDIT ON
+const bottom = document.querySelector(".bottom");
+const overlay = document.querySelector(".overlay");
+const count = 110;
+const size = 50;
+for (let i = 0; i <= count; i += 1) {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  bottom.appendChild(dot);
+}
+const dots = Array.from(document.querySelectorAll(".dot"));
+
+const updateText = (text) => {
+  Array.from(document.querySelectorAll(".text")).forEach(
+    (e) => (e.innerHTML = text)
+  );
+};
+
+const reset = () => {
+  dots.forEach((dot, i) => {
+    const x = (i / count) * (190 + size) - size / 2;
+    const y = Math.random(1) * 52 - size / 2;
+    dot.style.width = `${size}px`;
+    dot.style.height = `${size}px`;
+    dot.style.left = `${x}px`;
+    dot.style.top = `${y}px`;
+    dot.style.opacity = 1;
+    dot.style.transform = "scale(1)";
+  });
+};
+reset();
+
+overlay.addEventListener("click", () => {
+  anime({
+    easing: "linear",
+    targets: document.querySelectorAll(".dot"),
+    opacity: [{ value: 0, duration: 600, delay: anime.stagger(10) }],
+    translateX: {
+      value: function () {
+        return anime.random(-30, 30);
+      },
+      duration: 400,
+      delay: anime.stagger(10)
+    },
+    translateY: {
+      value: function () {
+        return anime.random(-30, 30);
+      },
+      duration: 400,
+      delay: anime.stagger(10)
+    },
+    scale: {
+      value: function () {
+        return 0;
+      },
+      duration: 400,
+      delay: anime.stagger(10)
+    }
+  });
+  anime({
+    easing: "linear",
+    delay: 4000,
+    complete: () => {
+      updateText("Submitted");
+      setTimeout(() => {
+        updateText("Submit");
+        reset();
+      }, 3000);
+    }
+  });
+});
